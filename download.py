577bb07c36ef1b6c03f0b435e91893245f5c83f5
xapharius <mihai.suteu@xapharius.com>
2017-10-04 23:04:01 +0100

    api time limit, preprocessing

--- download.py
+++ download.py
@@ -1,13 +1,9 @@
+import time
 import json
-import pandas as pd
-import re
 import requests
 import argparse
 import warnings
-
-
-def _price_to_int(s):
-    return int(re.sub('[$,]', '', s))
+import pandas as pd
 
 
 # It turns out Blue Nile's API will only let us grab 1000 diamonds
@@ -16,43 +12,62 @@ def _price_to_int(s):
 # 1. Get first thousand diamonds in query.
 # 2. Use diamond with highest price in that query to seed the next
 #    query.
+
+
+def process_entry(d):
+    return {
+        "carat": float(d["carat"][0]),
+        "clarity": d["clarity"][0],
+        "color": d["color"][0],
+        "culet": d["culet"][0],
+        "cut": d["cut"][0]["label"],
+        "date": d["date"][0],
+        "depth": float(d["depth"][0]),
+        "fluorscence": d["fluorescence"][0],
+        "id": d["id"],
+        "lxwRatio": float(d["lxwRatio"][0]),
+        "polish": d["polish"][0],
+        "price": int(float(d["price"][0][1:].replace(",", "."))),
+        #"pricePerCarat": float(d["pricePerCarat"][0][1:]),
+        "shapeName": d["shapeName"][0],
+        "symmetry": d["symmetry"][0],
+        "table": float(d["table"][0])
+    }
+
+
 def diamonds(params):
     assert params['sortColumn'] == 'price' and params['sortDirection'] == 'asc'
 
     landing_page = requests.get('http://www.bluenile.com/')
     url = 'http://www.bluenile.com/api/public/diamond-search-grid/v2'
     result = []
+    iteration = 1
     while True:
         response = requests.get(url, params, cookies=landing_page.cookies)
-        d = json.loads(response.text)
+        try:
+            d = json.loads(response.text)
+        except Exception as e:
+            print(response.text)
+            return result
+        
+        print("iteration: {}, diamonds left: {}".format(iteration, d['countRaw']))
+        iteration += 1
+
         last_page = params['pageSize'] >= d['countRaw']
 
-        for i in range(len(d['results'])):
-            d['results'][i]['price'] = _price_to_int(d['results'][i]['price'])
-        max_price = d['results'][-1]['price']
-        min_price = d['results'][0]['price']
+        it_results = [process_entry(entry) for entry in d["results"]]
+        max_price = it_results[-1]['price']
+        min_price = it_results[0]['price']
 
         if last_page:
-            result += d['results']
+            result += it_results
             break
         else:
             assert min_price < max_price, 'There are over %d diamonds with these characteristics at this price %d.' % (params['pageSize'], min_price)
-            result += [x for x in d['results'] if x['price'] < max_price]
+            result += [x for x in it_results if x['price'] < max_price]
             params['minPrice'] = max_price
+        time.sleep(60)  # api limit
     return result
-
-
-def clean(data):
-    # Put the data into a data frame.
-    df = pd.DataFrame(data)
-
-    # Clean up the data.
-    for col in ['carat', 'depth', 'lxwRatio', 'table']:
-        df[col] = df[col].map(lambda s: s.replace(',', '')).astype(float)
-    for col in ['pricePerCarat']:
-        df[col] = df[col].map(_price_to_int)
-
-    return df
 
 
 def parse_arguments():
@@ -104,18 +119,19 @@ def parse_arguments():
         parser.add_argument('--%s' % k, default=v, type=type(v))
 
     args = parser.parse_args()
-    d = args.__dict__
-    for k, v in d.items():
-        if v is None:
-            del d[k]
+    d = {k:v for k, v in args.__dict__.items() if v is not None}
     return d
 
 
 def main():
     params = parse_arguments()
     l = diamonds(params)
-    df = clean(l)
-    print df.to_csv(index=False)
+    if len(l) == 0:
+        print("No Data Downloaded")
+        return
+    df = pd.DataFrame(l)
+    file_name = str(pd.datetime.now()) + ".csv"
+    df.to_csv(file_name, index=False)
 
 
 if __name__ == '__main__':
